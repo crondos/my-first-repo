df38d7feaa107361efacea8f3fdc320bdd2e7baf
crondos <cfr8080@stern.nyu.edu>
2025-07-29 18:49:22 -0400

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,31 @@
+from google.colab import userdata
+PLAYER_NAME = userdata.get('PLAYER_NAME')
+print(f"Welcome {PLAYER_NAME}, to our Rock Paper Scissors Game!")
+
+valid_selections = ["rock", "paper", "scissors"]
+while True:
+    player_selection = input("Please select Rock, Paper, or Scissors: ").strip().lower()
+    if player_selection in valid_selections:
+        print(f"You chose: {player_selection}")
+        break
+    else:
+        print(f"So close {PLAYER_NAME}. Try again — type Rock, Paper, or Scissors.")
+
+import random
+
+computer_selection = random.choice(valid_selections)
+
+print(f"The computer chose: {computer_selection}")
+
+if player_selection == computer_selection:
+    result = "TIE! Please try again"
+elif (
+    (player_selection == "rock" and computer_selection == "scissors") or
+    (player_selection == "paper" and computer_selection == "rock") or
+    (player_selection == "scissors" and computer_selection == "paper")
+):
+    result = f"You Win! Lets play again?"
+else:
+    result = "Lenny Wins! Lets play again?"
+
+print(result)
